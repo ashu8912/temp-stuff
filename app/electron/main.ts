headlamp-k8s/headlamp

desktop: Allow to stop update notifications on user demand

app/electron/main.ts
@@ -302,6 +302,10 @@ function startElecron() {
       return { action: 'deny' };
     });
 
+    ipcMain.on('disable_update_checking', () => {
+      store.set('disable_update_check', true);
+    });
+
     mainWindow.webContents.on('dom-ready', () => {
       const octokit = new Octokit();
 
@@ -336,7 +340,10 @@ function startElecron() {
           store.set('app_version', appVersion);
         }
       }
-      fetchRelease();
+      const isUpdateCheckingDisabled = store.get('disable_update_check');
+      if (!isUpdateCheckingDisabled) {
+        fetchRelease();
+      }
     });
 
     mainWindow.on('closed', () => {

frontend/src/App.tsx
@@ -264,6 +264,15 @@ function UpdatePopup() {
           <Button color="secondary" onClick={() => window.open(updateDownloadURL)}>
             More
           </Button>
+          <Button
+            color="inherit"
+            onClick={() => {
+              desktopApi.send('disable_update_checking');
+              setShow(false);
+            }}
+          >
+            Do not notify again
+          </Button>
           <Button color="primary" onClick={() => setShow(false)}>
             Close
           </Button>

ashu8912/temp-stuff

app/electron/main.ts
@@ -303,6 +303,10 @@ function startElecron() {
       return { action: 'deny' };
     });
 
+    ipcMain.on('disableUpdateChecking', () => {
+      store.set('disable_update_check', true);
+    });
+
     mainWindow.webContents.on('dom-ready', () => {
       const octokit = new Octokit();
 
@@ -341,7 +345,10 @@ function startElecron() {
           store.set('app_version', appVersion);
         }
       }
-      fetchRelease();
+      const isUpdateCheckingDisabled = store.get('disable_update_check');
+      if (!isUpdateCheckingDisabled) {
+        fetchRelease();
+      }
     });
 
     mainWindow.on('closed', () => {

frontend/src/App.tsx
@@ -264,6 +264,15 @@ function UpdatePopup() {
           <Button color="secondary" onClick={() => window.open(updateDownloadURL)}>
             More
           </Button>
+          <Button
+            color="inherit"
+            onClick={() => {
+              desktopApi.send('disableUpdateChecking');
+              setShow(false);
+            }}
+          >
+            Do not notify again
+          </Button>
           <Button color="primary" onClick={() => setShow(false)}>
             Close
           </Button>