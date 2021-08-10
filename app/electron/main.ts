headlamp-k8s/headlamp

desktop: Don't show update available popup for flatpak

app/electron/main.ts
@@ -315,7 +315,11 @@ function startElecron() {
           owner: 'kinvolk',
           repo: 'headlamp',
         });
-        if (response.data.name >= appVersion && !response.data.name.startsWith('headlamp-helm')) {
+        if (
+          response.data.name >= appVersion &&
+          !response.data.name.startsWith('headlamp-helm') &&
+          !process.env.FLATPAK_ID
+        ) {
           mainWindow.webContents.send('update_available', {
             downloadURL: response.data.html_url,
           });
@@ -334,7 +338,10 @@ function startElecron() {
             owner: 'kinvolk',
             repo: 'headlamp',
           });
-          mainWindow.webContents.send('show_release_notes', { releaseNotes: response.data.body, appVersion });
+          mainWindow.webContents.send('show_release_notes', {
+            releaseNotes: response.data.body,
+            appVersion,
+          });
           // set the store version to latest so that we don't show release notes on
           // every start of app
           store.set('app_version', appVersion);

ashu8912/temp-stuff

app/electron/main.ts
@@ -317,11 +317,10 @@ function startElecron() {
           owner: 'kinvolk',
           repo: 'headlamp',
         });
-        const latestRelease = response.data.find((release) => !release.name.startsWith('headlamp-helm'))
-        if (
-          semver.gt(latestRelease.name, appVersion) &&
-          !process.env.FLATPAK_ID
-        ) {
+        const latestRelease = response.data.find(
+          release => !release.name.startsWith('headlamp-helm')
+        );
+        if (semver.gt(latestRelease.name, appVersion) && !process.env.FLATPAK_ID) {
           mainWindow.webContents.send('update_available', {
             downloadURL: latestRelease.html_url,
           });
@@ -340,7 +339,10 @@ function startElecron() {
             owner: 'kinvolk',
             repo: 'headlamp',
           });
-          mainWindow.webContents.send('showReleaseNotes', { releaseNotes: response.data.body, appVersion });
+          mainWindow.webContents.send('showReleaseNotes', {
+            releaseNotes: response.data.body,
+            appVersion,
+          });
           // set the store version to latest so that we don't show release notes on
           // every start of app
           store.set('app_version', appVersion);